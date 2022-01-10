ad62ec24532042435ac51223dd9ab0796374e241
Chen-Lab-CHAMP <97065755+Chen-Lab-CHAMP@users.noreply.github.com>
2022-01-10 10:55:25 -0500

    Add files via upload

---
+++ build_bmi_cshore/bmi_cshore_src/_build/CMakeFiles/3.21.3/CMakeFortranCompiler.cmake
@@ -0,0 +1,70 @@
+set(CMAKE_Fortran_COMPILER "/home/lingzhu/.conda/envs/bblz_bmicshore/bin/x86_64-conda-linux-gnu-gfortran")
+set(CMAKE_Fortran_COMPILER_ARG1 "")
+set(CMAKE_Fortran_COMPILER_ID "GNU")
+set(CMAKE_Fortran_COMPILER_VERSION "9.4.0")
+set(CMAKE_Fortran_COMPILER_WRAPPER "")
+set(CMAKE_Fortran_PLATFORM_ID "")
+set(CMAKE_Fortran_SIMULATE_ID "")
+set(CMAKE_Fortran_SIMULATE_VERSION "")
+
+
+set(CMAKE_Fortran_COMPILER_SYSROOT "/home/lingzhu/.conda/envs/bblz_bmicshore/bin/../x86_64-conda-linux-gnu/sysroot/usr")
+set(CMAKE_COMPILER_SYSROOT "/home/lingzhu/.conda/envs/bblz_bmicshore/bin/../x86_64-conda-linux-gnu/sysroot/usr")
+
+set(CMAKE_AR "/home/lingzhu/.conda/envs/bblz_bmicshore/bin/x86_64-conda-linux-gnu-ar")
+set(CMAKE_Fortran_COMPILER_AR "/home/lingzhu/.conda/envs/bblz_bmicshore/bin/x86_64-conda-linux-gnu-gcc-ar")
+set(CMAKE_RANLIB "/home/lingzhu/.conda/envs/bblz_bmicshore/bin/x86_64-conda-linux-gnu-ranlib")
+set(CMAKE_Fortran_COMPILER_RANLIB "/home/lingzhu/.conda/envs/bblz_bmicshore/bin/x86_64-conda-linux-gnu-gcc-ranlib")
+set(CMAKE_COMPILER_IS_GNUG77 1)
+set(CMAKE_Fortran_COMPILER_LOADED 1)
+set(CMAKE_Fortran_COMPILER_WORKS TRUE)
+set(CMAKE_Fortran_ABI_COMPILED TRUE)
+set(CMAKE_COMPILER_IS_MINGW )
+set(CMAKE_COMPILER_IS_CYGWIN )
+if(CMAKE_COMPILER_IS_CYGWIN)
+  set(CYGWIN 1)
+  set(UNIX 1)
+endif()
+
+set(CMAKE_Fortran_COMPILER_ENV_VAR "FC")
+
+set(CMAKE_Fortran_COMPILER_SUPPORTS_F90 1)
+
+if(CMAKE_COMPILER_IS_MINGW)
+  set(MINGW 1)
+endif()
+set(CMAKE_Fortran_COMPILER_ID_RUN 1)
+set(CMAKE_Fortran_SOURCE_FILE_EXTENSIONS f;F;fpp;FPP;f77;F77;f90;F90;for;For;FOR;f95;F95)
+set(CMAKE_Fortran_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_Fortran_LINKER_PREFERENCE 20)
+if(UNIX)
+  set(CMAKE_Fortran_OUTPUT_EXTENSION .o)
+else()
+  set(CMAKE_Fortran_OUTPUT_EXTENSION .obj)
+endif()
+
+# Save compiler ABI information.
+set(CMAKE_Fortran_SIZEOF_DATA_PTR "8")
+set(CMAKE_Fortran_COMPILER_ABI "")
+set(CMAKE_Fortran_LIBRARY_ARCHITECTURE "")
+
+if(CMAKE_Fortran_SIZEOF_DATA_PTR AND NOT CMAKE_SIZEOF_VOID_P)
+  set(CMAKE_SIZEOF_VOID_P "${CMAKE_Fortran_SIZEOF_DATA_PTR}")
+endif()
+
+if(CMAKE_Fortran_COMPILER_ABI)
+  set(CMAKE_INTERNAL_PLATFORM_ABI "${CMAKE_Fortran_COMPILER_ABI}")
+endif()
+
+if(CMAKE_Fortran_LIBRARY_ARCHITECTURE)
+  set(CMAKE_LIBRARY_ARCHITECTURE "")
+endif()
+
+
+
+
+
+set(CMAKE_Fortran_IMPLICIT_INCLUDE_DIRECTORIES "/home/lingzhu/.conda/envs/bblz_bmicshore/include;/home/lingzhu/.conda/envs/bblz_bmicshore/lib/gcc/x86_64-conda-linux-gnu/9.4.0/finclude;/home/lingzhu/publictools/include;/home/lingzhu/.conda/envs/bblz_bmicshore/lib/gcc/x86_64-conda-linux-gnu/9.4.0/include;/home/lingzhu/.conda/envs/bblz_bmicshore/lib/gcc/x86_64-conda-linux-gnu/9.4.0/include-fixed;/home/lingzhu/.conda/envs/bblz_bmicshore/x86_64-conda-linux-gnu/include;/home/lingzhu/.conda/envs/bblz_bmicshore/x86_64-conda-linux-gnu/sysroot/usr/include")
+set(CMAKE_Fortran_IMPLICIT_LINK_LIBRARIES "gfortran;m;gomp;gcc_s;gcc;quadmath;m;gcc_s;gcc;pthread;c;gcc_s;gcc")
+set(CMAKE_Fortran_IMPLICIT_LINK_DIRECTORIES "/home/lingzhu/.conda/envs/bblz_bmicshore/lib;/home/lingzhu/.conda/envs/bblz_bmicshore/lib/gcc/x86_64-conda-linux-gnu/9.4.0;/home/lingzhu/.conda/envs/bblz_bmicshore/lib/gcc;/home/lingzhu/publictools/lib;/home/lingzhu/.conda/envs/bblz_bmicshore/x86_64-conda-linux-gnu/lib;/home/lingzhu/.conda/envs/bblz_bmicshore/x86_64-conda-linux-gnu/sysroot/lib;/home/lingzhu/.conda/envs/bblz_bmicshore/x86_64-conda-linux-gnu/sysroot/usr/lib")
+set(CMAKE_Fortran_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
---
+++ build_bmi_cshore/bmi_cshore_src/_build/cshore/CMakeFiles/run_cshore.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run_cshore.dir/main.f90.o"
+  "run_cshore"
+  "run_cshore.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/run_cshore.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
